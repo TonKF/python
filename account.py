4c75ab1de2c40fa28f12c553b987f1e48023b7cb
TonKF <67007620+TonKF@users.noreply.github.com>
2020-06-17 11:43:18 +0900

    Create account.py

---
+++ account.py
@@ -0,0 +1,20 @@
+import gspread
+import subprocess
+
+from oauth2client.service_account import ServiceAccountCredentials
+scope = [
+'https://spreadsheets.google.com/feeds',
+'https://www.googleapis.com/auth/drive',
+]
+json_file_name = 'py.json'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
+gc = gspread.authorize(credentials)
+spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1bn_3dljif7wb4IDjwUe34TSSOuqZ6w7Zcgb-8OSafNI/edit#gid=0'
+
+doc = gc.open_by_url(spreadsheet_url)
+
+worksheet = doc.worksheet('sheet1')
+
+RAW = subprocess.check_output("cat ~/python/raw.log",shell=True)
+worksheet.update_acell('L7', RAW)
+
